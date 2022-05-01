ad34fe23cb392da720b1c5ba8908bb60a951f215
Denis Lleshi <Denislleshi62@gmail.com>
2022-05-01 22:48:34 +0200

    unversioned files

---
+++ AuctionsDenis/ScheduleTask/ExpiredProductSchedule.cs
@@ -0,0 +1,32 @@
+﻿using AuctionsDenis.BackgroundService;
+using System;
+using System.Threading.Tasks;
+using AuctionsDenis.BackgroundService;
+using AuctionsDenis.Service.ProductService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionsDenis.ScheduleTask
+{
+    public class ExpiredProductSchedule : ScheduledProcessor
+    {
+
+        public ExpiredProductSchedule(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+        {
+                
+        }
+
+        protected override string Schedule => "*/1 * * * *"; // every 1 min 
+
+        public override async Task ProcessInScope(IServiceProvider scopeServiceProvider)
+        {
+            IProductService productService = scopeServiceProvider.GetRequiredService<IProductService>();
+            productService.ExpiredProduct();
+            // return Task.CompletedTask;
+
+
+            await Task.Run(() => {
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
